4686a52af8335a8bd6b61a32f01abf914eb33edd
Sachitha Maheshkumar <155910259+SachCodeCraft@users.noreply.github.com>
2024-04-30 15:32:23 +0530

    Add files via upload

---
+++ add.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 4;
+        int b = 9;
+        int sum = a + b;
+        System.out.println("The sum of " + a + " and " + b + " is: " + sum);
+    }
+}
